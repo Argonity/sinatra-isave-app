018314766a384dba6f0c3e714b0d9678e76a34b7
Argonity <maurice.argoetti@gmail.com>
2019-03-07 21:42:03 -0500

    add get signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
   
+  #loads the signup page
+  #see User Authentication in Sinatra lab
+  get '/signup' do
+    if logged_in?
+      redirect to '/savings_account'
+    else
+      erb :'/users/create_user'
+    end
+  end  
+
+
+
+
 end

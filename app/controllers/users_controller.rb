39db7076130ccd5f20fe9159308f63f9b02d39f3
Argonity <maurice.argoetti@gmail.com>
2019-03-07 22:48:19 -0500

    add post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,14 @@ class UsersController < ApplicationController
     end
   end  
 
-  #POST action to retrieve new user info from signup form
+  #POST action to retrieve new user from signup form
   post '/signup' do #users signup with email and password only
     if params[:email] == "" || params[:password] == "" #check if email and password fields are blank
       redirect to '/signup'
     else
       @user = User.new(email: params[:email], password: params[:password]) #retrieve email and password from form and create new user 
       @user.save
-      session[:user_id] = @user.id #user is now logged in
+      session[:user_id] = @user.id #login the user by enabling a session
       redirect to '/savings_account' #redirect logged in user to savings account route
     end
   end
@@ -29,5 +29,17 @@ class UsersController < ApplicationController
       redirect '/savings_account' #redirect logged in user to savings account route
     end
   end
+
+  #POST action to find existing user from login form
+  post '/login' do
+    @user = User.find_by(name: params[:name]) #find user by name
+    if @user && @user.authenticate(params[:password]) #if user name and password found
+      session[:user_id] = @user.id #login the user by enabling a session
+      redirect '/savings_account'
+    else
+      redirect '/signup' #redirect to signup route if user not found
+    end
+  end
+
 
 end

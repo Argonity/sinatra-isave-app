624df6fafd6b5b8a133ee868be21a22b5c3e57fa
Argonity <maurice.argoetti@gmail.com>
2019-03-09 21:03:16 -0500

    add post action to create new savings account

--- app/controllers/savings_accounts_controller.rb
+++ app/controllers/savings_accounts_controller.rb
@@ -19,5 +19,16 @@ class SavingsAccountsController < ApplicationController
     end
   end
 
+  #POST action to create a new savings account
+  post '/savings' do
+    if params[:item] == "" || params[:price] == "" || params[:amount_saved] == "" || params[:priority_level] == ""
+      redirect '/savings/new'
+    else
+      @user = User.find_by_id(session[:user_id])
+      SavingsAccount.create(item: params[:item], price: params[:price], amount_saved: params[:amount_saved], priority_level: params[:priority_level], user_id: @user.id)
+    end
+    redirect '/savings'
+  end
+
 
 end

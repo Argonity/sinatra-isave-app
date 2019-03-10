a72dbe033f7030827863f7efbb820016e6c49070
Argonity <maurice.argoetti@gmail.com>
2019-03-09 19:14:53 -0500

    add get savings account route

--- app/controllers/savings_accounts_controller.rb
+++ app/controllers/savings_accounts_controller.rb
@@ -1,3 +1,15 @@
 class SavingsAccountsController < ApplicationController
   
+  #GET action to render all savings accounts for existing user 
+  get '/savings_account' do
+    if logged_in?
+      @savings = SavingsAccount.all
+      erb :'/savings_accounts/index'
+    else
+      redirect '/login'
+    end
+  end
+
+
+
 end

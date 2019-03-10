d395476cfe196f4ee86a31ba18429f0c967ae59a
Argonity <maurice.argoetti@gmail.com>
2019-03-09 19:27:04 -0500

    update route 'savings_account' to 'savings' to avoid confusion with folder names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   #GET action to render signup form
   get '/signup' do
     if logged_in? #check if user is logged in with helper method
-      redirect to '/savings_account' #redirect logged in user to savings account route
+      redirect to '/savings' #redirect logged in user to savings account route
     else
       erb :'/users/create_user' 
     end
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
       @user = User.new(email: params[:email], password: params[:password]) #retrieve email and password from form and create new user 
       @user.save
       session[:user_id] = @user.id #login the user by enabling a session
-      redirect to '/savings_account' #redirect logged in user to savings account route
+      redirect to '/savings' #redirect logged in user to savings account route
     end
   end
 
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     if !logged_in? #check if user is not logged in with helper method
       erb :'/users/login' #render login page
     else
-      redirect '/savings_account' #redirect logged in user to savings account route
+      redirect '/savings' #redirect logged in user to savings account route
     end
   end
 
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     @user = User.find_by(name: params[:name]) #find user by name
     if @user && @user.authenticate(params[:password]) #if user name and password found
       session[:user_id] = @user.id #login the user by enabling a session
-      redirect '/savings_account'
+      redirect '/savings'
     else
       redirect '/signup' #redirect to signup route if user not found
     end
